0eddbd8348863b274de6a106490690255700dad0
prathamesh-mathapatik <prathamesh20@navgurukul.org>
2024-08-23 17:48:01 +0530

    Working on resend

--- src/lip/resen.ts
+++ src/lip/resen.ts
@@ -1,6 +1,5 @@
 import { Resend } from 'resend';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import  VerificationEmail  from '../../emails/EmailTemplate';
-console.log(process.env.RESEND_API_KEY,"process.env.RESEND_API_KEY")
 export const resend = new Resend(process.env.RESEND_API_KEY);
 
